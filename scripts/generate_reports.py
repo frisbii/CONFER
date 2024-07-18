d0710feaacd9ba70dd4f0c13a4fb9ac9723d7e8f
madi <madigudin@gmail.com>
2024-07-18 18:28:18 -0400

    sad face

--- scripts/generate_reports.py
+++ scripts/generate_reports.py
@@ -1,5 +1,6 @@
 import datetime
 from pathlib import Path
+import subprocess
 from globals import PROJECT_ROOT, CONFIG
 
 TIME : str
@@ -9,19 +10,18 @@ LOG_FOLDER : Path
 RPT_FOLDER : Path
 PRJ_FOLDER : Path
 
-def startup():
-    now  : datetime.datetime = datetime.datetime.now()
-    TIME = f"{now.year}-{now.month}-{now.day}_{now.hour}-{now.minute}-{now.second}"
+now  : datetime.datetime = datetime.datetime.now()
+TIME = f"{now.year}-{now.month}-{now.day}_{now.hour}-{now.minute}-{now.second}"
 
-    RUN_FOLDER = PROJECT_ROOT / 'saved' / TIME
-    LOG_FOLDER = RUN_FOLDER / 'log'
-    RPT_FOLDER = RUN_FOLDER / 'rpt'
-    PRJ_FOLDER = RUN_FOLDER / 'prj'
+RUN_FOLDER = PROJECT_ROOT / 'saved' / TIME
+LOG_FOLDER = RUN_FOLDER / 'log'
+RPT_FOLDER = RUN_FOLDER / 'rpt'
+PRJ_FOLDER = RUN_FOLDER / 'prj'
 
-    Path.mkdir(RUN_FOLDER)
-    Path.mkdir(LOG_FOLDER)
-    Path.mkdir(RPT_FOLDER)
-    Path.mkdir(PRJ_FOLDER)
+Path.mkdir(RUN_FOLDER)
+Path.mkdir(LOG_FOLDER)
+Path.mkdir(RPT_FOLDER)
+Path.mkdir(PRJ_FOLDER)
 
 
 
@@ -39,9 +39,18 @@ def process_paramaterization(
         'PRJ_NAME' : dow,
         'SRC_FILE' : Path.as_posix(PROJECT_ROOT / 'src' / f'{d}.cpp'),
         'HEADER_FILE' : Path.as_posix(PROJECT_ROOT / 'src' / 'params.hpp'),
-        'CFLAGS' : ' '.join(CONFIG.cflags + [f'-D{o}', f'-DWIDTH={w}']), # type: ignore
-        'PART' : CONFIG.part, # type: ignore
+        'CFLAGS' : ' '.join(CONFIG['cflags'] + [f'-D{o}', f'-DWIDTH={w}']), 
+        'PART' : CONFIG['part'],
         'CLOCK_PERIOD' : '10'
     }
 
-    
+    subprocess.run(
+        [
+            CONFIG['vivadohls_install_path'],
+            '-f', str(PROJECT_ROOT / 'scripts' / 'vhls_generate_ip.tcl'),
+            '-l', str(LOG_FOLDER / f'{dow}_vhls.log')
+        ]
+    )
+
+
+process_paramaterization('uint', 'ADD', 4)
