1704aaef3ac87caad168ea53047f6b632096b406
Jesse Looney <42242696+jesselooney@users.noreply.github.com>
2024-07-31 14:15:14 -0400

    Variable x-axis API

--- scripts/read_data.py
+++ scripts/read_data.py
@@ -1,9 +1,10 @@
 import argparse
 import itertools
 import pandas as pd
-from globals import PROJECT_ROOT
+from globals import PROJECT_ROOT, CONFIG
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 from matplotlib.container import BarContainer
 import mplcursors
 
@@ -16,31 +17,33 @@ def show_annotation(sel: mplcursors.Selection):
         
 def generate_plots():
     global df
-    # define total design space (operation, datatype, part, period)
-    operations = ['ADD', 'MUL']
-    datatypes = ['uint', 'sint', 'ufxp', 'sfxp', 'float', 'posit']
-    parts = ['xc7k160tfbg484-1', 'xc7s50csga324-1']
-    periods = [3, 10, 1000000]
-    # choose which parameters to hold fixed for this run
-    specified_params = [
-        ['ADD'], 
-        ['uint', 'float', 'posit'], 
-        ["xc7s50csga324-1"], 
-        [3]
-    ]
-    # fill the list of desired plots for this run
-    parameters = list(itertools.product(
-        specified_params[0] if specified_params[0] is not None else operations, 
-        specified_params[1] if specified_params[1] is not None else datatypes, 
-        specified_params[2] if specified_params[2] is not None else parts, 
-        specified_params[3] if specified_params[3] is not None else periods, 
-        ))
+
+    # defines the order and available variables
+    variables = ['operation', 'part', 'datatype', 'width', 'period'] # last one is x-axis
+    variable_values = {
+        'operation': ['MUL'],
+        'datatype': ['float'],
+        'part': ['xc7s50csga324-1'],
+        'width': [16],
+        'period': [3, 10, 100, 1000]
+    }
+    # where empty means use every available value (in dataframe? or in given ordered list?)
+
+    # Compute the list of full parameterizations to visualize this run.
+    # TODO: parameterizations is the wrong word for this purpose.
+    # TODO: the getattr() call is a janky hack.
+    parameterizations = list(itertools.product(
+        *[variable_values.get(var, getattr(CONFIG, var + 's')) for var in variables[:-1]]
+    ))
+
     # create figure and prepare subfigures
     fig = plt.figure()
-    sfigs = fig.subfigures(1, len(parameters))
+    sfigs = fig.subfigures(1, len(parameterizations), squeeze=False)[0]
+
+    df = df.reset_index().set_index(variables)
 
     # VISUALIZATION
-    for i, p in enumerate(parameters):
+    for i, p in enumerate(parameterizations):
         # Create figure and axes
         subfig = sfigs[i]
         axs = sfigs[i].subplots(3, 1, sharex=True)
@@ -53,51 +56,30 @@ def generate_plots():
             axs[0].set_ylabel("Primitives count")
             axs[1].set_ylabel("Delay (ns)")
             axs[2].set_ylabel("Power usage (W)")
-        axs[2].set_xlabel("Bit width")
+        axs[2].set_xlabel(variables[-1])
 
-        ##############
-        # PRIMITIVES
-        # create specific table
-        try:
-            d = df.loc[*p].drop(['delay_route', 'delay_logic', 'power_static', 'power_dynamic'], axis=1).copy() # type: ignore
-        except:
-            continue
-        # bottom_values necessary to keep track of stacked baselines
-        bottom_values = [0] * len(d)
-        for col in d.columns:
-            # plot each column on top of each other
-            axs[0].bar(d.index, d[col], bottom=bottom_values, label=col, width=2)
-            # add the new values to the bottom_values baseline to prepare for the next column
-            bottom_values = [i+j for i, j in zip(bottom_values, d[col])]
-        # styling
-        axs[0].legend()
-
-        ##########
-        # TIMING
-        d = df.loc[*p].loc[:, ['delay_route', 'delay_logic']].copy() # type: ignore
-        bottom_values = [0] * len(d)
-        for col in d.columns:
-            # plot each column on top of each other
-            axs[1].bar(d.index, d[col], bottom=bottom_values, label=col, width=2)
-            # add the new values to the bottom_values baseline to prepare for the next column
-            bottom_values = [i+j for i, j in zip(bottom_values, d[col])]
-        axs[1].legend()
-
-        #########
-        # POWER
-        d = df.loc[*p].loc[:, ['power_dynamic']].copy() # type: ignore
-        bottom_values = [0] * len(d)
-        for col in d.columns:
-            # plot each column on top of each other
-            axs[2].bar(d.index, d[col], bottom=bottom_values, label=col, width=2)
-            # add the new values to the bottom_values baseline to prepare for the next column
-            bottom_values = [i+j for i, j in zip(bottom_values, d[col])]
-        axs[2].legend()
+        # Utilization
+        stack_bars(axs[0], df.loc[p].drop(['delay_route', 'delay_logic', 'power_static', 'power_dynamic'], axis=1).copy())
+        # Timing
+        stack_bars(axs[1], df.loc[p].loc[:, ['delay_route', 'delay_logic']].copy())
+        # Power
+        stack_bars(axs[2], df.loc[p].loc[:, ['power_dynamic']].copy())
     
     cursor = mplcursors.cursor(hover=True)
     cursor.connect('add', show_annotation)  
 
     plt.show()
+
+
+def stack_bars(ax: Axes, df: pd.DataFrame):
+    bottom_values = [0] * len(df)
+    for col in df.columns:
+        # plot each column on top of each other
+        ax.bar(range(len(df.index)), df[col], bottom=bottom_values, label=col, width=1)
+        ax.set_xticks(range(len(df.index)), df.index)
+        # add the new values to the bottom_values baseline to prepare for the next column
+        bottom_values = [i+j for i, j in zip(bottom_values, df[col])]
+    ax.legend()
 
 
 def main():
